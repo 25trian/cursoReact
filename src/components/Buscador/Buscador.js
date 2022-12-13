c134c8837b301b152c558d20f700e69112f6630e
maricaroj <juarezmariana18@gmail.com>
2022-12-13 17:50:51 -0300

    cambios en los estilos, elimino archivos no utilizados

--- src/components/Buscador/Buscador.js
+++ src/components/Buscador/Buscador.js
@@ -1,16 +1,16 @@
 import React, { useRef, useState } from "react"; //Actualizar sin recargar pagina (Creo)
-import { Button, Card, Form, InputGroup } from "react-bootstrap"; //Elementos de Boostrap
-import { Link } from "react-router-dom"; 
+import { Card, Form, InputGroup, Col } from "react-bootstrap"; //Elementos de Boostrap
+import { Link } from "react-router-dom";
 //El componente buscador consume la api desde "https://api.lyrics.ovh"
 export const Buscador = () => {
   const [resultadoBusquedaCanciones, setResultadoBusquedaCanciones] =
     useState();
   const [letraCancion, setLetraCancion] = useState();
-//API
+  //API
   const API_URL = "https://api.lyrics.ovh";
 
   const valorInputBusqueda = useRef();
-// usamos fetch para consumir la api
+  // usamos fetch para consumir la api
   const getLyrics = async (artist, songTilte) => {
     return new Promise(async (resolve, reject) => {
       try {
@@ -22,7 +22,7 @@ export const Buscador = () => {
       }
     });
   };
-// usamos fetch para traer las canciones
+  // usamos fetch para traer las canciones
   const searchSongs = async (value) => {
     const res = await fetch(`${API_URL}/suggest/${value}`);
     const data = await res.json();
@@ -30,12 +30,12 @@ export const Buscador = () => {
 
     console.log(resultadoBusquedaCanciones);
   };
-//Dibujamos el fomulario donde se escriba la canción que buscamos
+  //Dibujamos el fomulario donde se escriba la canción que buscamos
   return (
-    <div>
-      <InputGroup className="mb-3 mt-5">
+    <>
+      <InputGroup size="lg" className="mb-3 mt-5">
         <Form.Control
-          placeholder="Nombre de la cancion"
+          placeholder="Escribe el nombre del artista o la canción que buscas"
           aria-label="Nombre de la cancion"
           aria-describedby="basic-addon2"
           ref={valorInputBusqueda}
@@ -47,36 +47,45 @@ export const Buscador = () => {
 
       {resultadoBusquedaCanciones &&
         resultadoBusquedaCanciones.map((item) => {
-          return ( //Mostramos en formato tarjeta las canciones: El título, autor, preview y el link hacia la cancion completa.
-            <Card key={item.id} className="mt-2 shadow">
-              <Card.Header>
-                {item.title} - {item.artist.name}{" "}
-              </Card.Header>
-              <Card.Body>
-                <blockquote className="blockquote mb-0 row">
-                  <img
-                    src={item.album.cover}
-                    alt={item.album.title}
-                    className="col-2 mb-2"
-                  />
-                  <div className="col-4">
-                    <p className="col-12">Escuchar preview</p>
-                    <audio id="audio" controls className="col-6">
-                      <source type="audio/wav" src={item.preview} />
-                    </audio>
-                  </div>
-                  <div className="col-6"></div>
-                  <footer className="blockquote-footer col-12">
-                    <a href={item.link} target="_blank" rel="noreferrer">
-                      Escucha la cancion
-                      <cite title="Source Title"> Completa</cite>
-                    </a>
-                  </footer>
-                </blockquote>
-              </Card.Body>
-            </Card>
+          return (
+            //Mostramos en formato tarjeta las canciones: El título, autor, preview y el link hacia la cancion completa.
+            <Col sm={12}>
+              <Card
+                key={item.id}
+                className="shadow my-2"
+                bg="dark"
+                text="white"
+              >
+                <Card.Header as={"h5"} style={{ height: "3.7rem" }}>
+                    {item.title} - {item.artist.name}
+                </Card.Header>
+                <Card.Body>
+                  <Card.Text className="blockquote mb-0 row">
+                    <img
+                      src={item.album.cover}
+                      alt={item.album.title}
+                      className="col-4 mb-2"
+                    />
+                    <div className="col-8">
+                      <p className="col-12" style={{ textAlign: "center" }}>
+                        Escuchar preview
+                      </p>
+                      <audio id="audio" controls className="col-12">
+                        <source type="audio/wav" src={item.preview} />
+                      </audio>
+                    </div>
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer className="col-12">
+                  <Card.Link href={item.link} target="_blank" rel="noreferrer">
+                    Escucha la canción
+                    <cite title={item.title}> Completa</cite>
+                  </Card.Link>
+                </Card.Footer>
+              </Card>
+            </Col>
           );
         })}
-    </div>
+    </>
   );
 };
